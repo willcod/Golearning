712e6126763d989d656e73f0a15fd9c06cebd9e9
willcod <william.wow@icloud.com>
2021-05-06 17:21:58 +0800

    finished 3rd homework

--- concurrency/homework/main.go
+++ concurrency/homework/main.go
@@ -6,11 +6,12 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func startHttpServer(wg *sync.WaitGroup) *http.Server {
+func startHttpServer() *http.Server {
 	server := &http.Server{Addr: ":8080"}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -34,21 +35,33 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	server := startHttpServer(&wg)
+	g, ctx := errgroup.WithContext(context.Background())
+	server := startHttpServer()
+	g.Go(func() error {
 
-	s := <-sig
-	switch s {
-	case syscall.SIGTERM:
-		fmt.Println("Received SIGTERM")
-		wg.Done()
-	case syscall.SIGINT:
-		fmt.Println("Received SIGINT")
-		wg.Done()
+		select {
+		case <-ctx.Done():
+			fmt.Println("Canceled")
+			return nil
+		default:
+			s := <-sig
+			switch s {
+			case syscall.SIGTERM:
+				fmt.Println("Received SIGTERM")
+				return fmt.Errorf("SIGTERM")
+
+			case syscall.SIGINT:
+				fmt.Println("Received SIGINT")
+				return fmt.Errorf("SIGINT")
+			}
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Printf("%v\n", err)
 	}
 
-	wg.Wait()
 	if err := server.Shutdown(context.Background()); err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
